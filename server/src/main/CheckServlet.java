2a37467937740b96a70c8598c137d0d9161183dd
Pavel Sytko <entropiagony@gmail.com>
2020-03-23 19:20:43 +0300

    80% DONE - servlets task

---
+++ server/src/main/CheckServlet.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CheckServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getOutputStream().println("{\"sucesss\" : true}");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().println("Try using post request");
+    }
+}
---
+++ server/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: voda
+  Date: 23.03.2020
+  Time: 2:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>I hate html and css</title>
+  </head>
+  <body>
+  Java Web Application
+  </body>
+</html>
