8a3c14acf04052590e34aa89074bc90cc8bdb051
Pavel Sytko <entropiagony@gmail.com>
2020-03-23 19:56:13 +0300

    100% DONE - added filter and redirect tasks

---
+++ server/src/main/TimeFilter.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class TimeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        long start = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long time = start - System.currentTimeMillis();
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String path = httpServletRequest.getRequestURI();
+        String method = httpServletRequest.getMethod();
+
+        System.out.println(method + " - " + path + " - " + time + "ms");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
